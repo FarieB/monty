8b07c3fd95c04dcdf5dff5cb37b1527aa67a78ba
FarieB <faraibande@gmail.com>
2023-12-14 23:24:13 -0800

    monty

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+/* ... (rest of the code) */
+
+(void)argc;
+(void)argv;
+
+return 0;  /* Use 0 instead of EXIT_SUCCESS */
+
+/* ... (rest of the code) */
+}
+
